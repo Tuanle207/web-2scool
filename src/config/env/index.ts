65c7622cc11e9e20dfc5fbf7d8060fc98fdff48e
Tuanle207 <letgo237@gmail.com>
2022-02-20 15:10:48 +0700

    Update env setting

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -2,11 +2,11 @@ import { IEnvConfig, EnvType } from './type';
 
 const ENV: IEnvConfig = {
   env: EnvType.Development,
-  host: 'http://147.182.219.63:5002',
+  host: 'http://147.182.219.63:5000',
   appUrl: 'http://147.182.219.63',
   oAuthConfig: {
-    issuer: 'http://147.182.219.63:5002',
-    clientId: 'Scool_App_Main',
+    issuer: 'http://147.182.219.63:5000',
+    clientId: 'Scool_App',
     responseType: 'code',
     clientSecret: '1q2w3e*',
     scope: 'offline_access openid profile role email phone Scool',
