ed5597e7f0110460201f05eed0255ac14901f2c8
Tuanle207 <letgo237@gmail.com>
2022-02-20 12:45:45 +0700

    Alter temporarily code for testin CORS

--- src/config/axios/interceptors.ts
+++ src/config/axios/interceptors.ts
@@ -10,6 +10,7 @@ import { isTokenExpired } from './util';
 export const configHttpRequest = (axios: AxiosInstance) => {
   axios.interceptors.request.use(async (config: AxiosRequestConfig) => {
     const url = config.url || '';
+    console.log({url});
     if ([
       '/connect/token',
     ].includes(url)) {
@@ -22,12 +23,12 @@ export const configHttpRequest = (axios: AxiosInstance) => {
     config.headers['Authorization'] = `Bearer ${token}`;
 
     // Get new access token in case it expires
-    if (issuedAt && refreshToken && isTokenExpired(issuedAt, expiresIn)) {
-      const refreshRes = await AuthService.refresh(refreshToken);
-      let newToken = refreshRes.access_token;
-      config.headers['Authorization'] = `Bearer ${newToken}`;
-      redux.store.dispatch(AuthActions.setLogin(refreshRes));
-    }
+    // if (issuedAt && refreshToken && isTokenExpired(issuedAt, expiresIn)) {
+    //   const refreshRes = await AuthService.refresh(refreshToken);
+    //   let newToken = refreshRes.access_token;
+    //   config.headers['Authorization'] = `Bearer ${newToken}`;
+    //   redux.store.dispatch(AuthActions.setLogin(refreshRes));
+    // }
     
     config.headers['Accept-Language'] = 'vi-vn'
     config.headers['RequestVerificationToken'] = Cookies.get('XSRF-TOKEN');
--- src/config/env/deployment.ts
+++ src/config/env/deployment.ts
@@ -3,14 +3,14 @@ import { IEnvConfig, EnvType } from './type';
 const ENV: IEnvConfig = {
   env: EnvType.Development,
   host: '<API_URL>',
-    appUrl: '<APP_URL>',
-    oAuthConfig: {
-      issuer: '<AUTH_URL>',
-      clientId: '<CLIENT_ID>',
-      clientSecret: '<CLIENT_SECRET>',
-      responseType: 'code',
-      scope: 'offline_access Scool',
-    },
+  appUrl: '<APP_URL>',
+  oAuthConfig: {
+    issuer: '<AUTH_URL>',
+    clientId: '<CLIENT_ID>',
+    clientSecret: '<CLIENT_SECRET>',
+    responseType: 'code',
+    scope: 'offline_access Scool',
+  },
   enableLogger: false
 };
 
